51b96bd1a9d2528779fbcb63a6b24e2498fd390f
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-19 12:20:58 -0500

    Changed inventory trigger from collision to click from range

---
+++ ClickManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    private Transform t;
+    private Transform player;
+    public GameObject clicked;
+
+    void Awake() {
+
+    }
+
+    void Update () {
+            if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+                if (hit.collider != null) {
+                    clicked = hit.collider.gameObject;
+                    //Debug.Log(hit.collider.gameObject.name);
+                    t = hit.collider.gameObject.transform;
+                    player = GameObject.FindGameObjectWithTag("Player").transform;
+                    //print(player.name + " is " + Distance().ToString() + " units from " + hit.collider.gameObject.name);
+            }
+        }
+    }
+
+    public float Distance()
+     {
+         return Vector3.Distance(t.position, player.position);
+     }
+}
--- Pickup.cs
+++ Pickup.cs
@@ -6,25 +6,60 @@ public class Pickup : MonoBehaviour
 {
     private Inventory inventory;
     public GameObject itemButton;
+    public GameObject clicked_Object;
+    private GameObject player;
+    private float distance;
+    public float pickup_distance;
 
     private void Start()
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
-            Debug.Log("collision!");
-                for (int i= 0; i < inventory.slots.Length; i++)
-                {
-                    if (inventory.isFull[i] == false) {
-                        //ITEM CAN BE ADDED
-                        inventory.isFull[i] = true;
-                        Instantiate(itemButton, inventory.slots[i].transform, false); // false = not world coordinates
-                        Destroy(gameObject);
-                        break;
-                    }
+    void Update() {
+        clicked_Object = GameObject.FindGameObjectWithTag("ClickManager").GetComponent<ClickManager>().clicked;
+        if (clicked_Object == gameObject){
+            checkClicked();
+        }
+    }
+
+    // void OnTriggerEnter2D(Collider2D other) {
+    //     if (other.CompareTag("Player")) {
+    //         Debug.Log("collision!");
+    //         Debug.Log(clicked_Object);
+    //             for (int i= 0; i < inventory.slots.Length; i++)
+    //             {
+    //                 if (inventory.isFull[i] == false) {
+    //                     //ITEM CAN BE ADDED
+    //                     inventory.isFull[i] = true;
+    //                     Instantiate(itemButton, inventory.slots[i].transform, false); // false = not world coordinates
+    //                     Destroy(gameObject);
+    //                     break;
+    //                 }
+    //             }
+    //     }
+    // }
+
+    void checkClicked() {
+        Debug.Log(clicked_Object.name);
+        distance = GameObject.FindGameObjectWithTag("ClickManager").GetComponent<ClickManager>().Distance();
+        print("distance is "+ distance + " from " + clicked_Object);
+        if (distance < 2){
+            for (int i= 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false) {
+                     //ITEM CAN BE ADDED
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false); // false = not world coordinates
+                    Destroy(gameObject);
+                    break;
                 }
+            }
+        }
+        else{
+            GameObject.FindGameObjectWithTag("ClickManager").GetComponent<ClickManager>().clicked = null;
         }
     }
 }
+

8f82c6390b9d5039b3707fdd853feebf75d946b4
Julia Grimes <julia.s.grimes@gmail.com>
2023-09-23 00:01:37 -0400

    Added distance setting to door openable'

--- DoorBehavior.cs
+++ DoorBehavior.cs
@@ -7,18 +7,24 @@ public class DoorBehavior : MonoBehaviour
 
     private bool openable = false;
     private SceneSwitch _sceneManager;
+
+    public float pickup_distance;
     private UIManager _uiManager;
+
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         _uiManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
         _sceneManager = GameObject.Find("GameManager").GetComponent<SceneSwitch>();
+        player = GameObject.FindGameObjectWithTag("Player");
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
 
     }
 
@@ -27,7 +33,18 @@ public class DoorBehavior : MonoBehaviour
         //add menu here to confirm leaving the room? or animation with the door swinging open, on hover message "go to ..."
         if (openable == true)
         {
-            _sceneManager.GoInDoor();
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            print("distance is " + distance + " from door");
+            if (distance < pickup_distance)
+            {
+
+                _sceneManager.GoInDoor();
+
+            }
+            else
+            {
+            }
+
         }
         else
         {

1e672bcf6dd59d7dfafde84292d51bd62484c0fc
Julia Grimes <julia.s.grimes@gmail.com>
2023-10-03 22:27:09 -0400

    Troubleshooting look, random and adding button to reset easily to see what they look like with more iterations faster

--- Table/TeaLeaf.cs
+++ Table/TeaLeaf.cs
@@ -8,6 +8,13 @@ public class TeaLeaf : MonoBehaviour
     private int _numTeaLeaves = 0;
     private int _numTeaLeaves2 = 0;
     private int _numTeaLeaves3 = 0;
+    private int _startNumTeaLeaves2 = 0;
+
+    private int _startNumTeaLeaves = 0;
+
+    private int _startNumTeaLeaves3 = 0;
+
+
     [SerializeField]
     private GameObject _leafContainer3;
 
@@ -21,9 +28,12 @@ public class TeaLeaf : MonoBehaviour
     [SerializeField]
     private GameObject _teaLeaves2;
     // Start is called before the first frame update
+
     void Start()
     {
-
+        _startNumTeaLeaves = Random.Range(10, 200);
+        _startNumTeaLeaves2 = Random.Range(10, 60);
+        _startNumTeaLeaves3 = Random.Range(100, 300);
     }
 
     // Update is called once per frame
@@ -32,37 +42,61 @@ public class TeaLeaf : MonoBehaviour
         startSpawn();
     }
 
+    public void resetSpawn()
+    {
+        foreach (Transform child in _leafContainer.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (Transform child in _leafContainer2.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (Transform child in _leafContainer3.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        _numTeaLeaves = 0;
+        _numTeaLeaves2 = 0;
+        _numTeaLeaves3 = 0;
+        _startNumTeaLeaves = Random.Range(10, 200);
+        _startNumTeaLeaves2 = Random.Range(10, 60);
+        _startNumTeaLeaves3 = Random.Range(100, 300);
+        startSpawn();
+
+    }
     public void startSpawn()
     {
 
-        while (_numTeaLeaves < 90)
+        while (_numTeaLeaves < _startNumTeaLeaves)
         {
 
             GameObject newEnemy = Instantiate(_teaLeaves, (Random.insideUnitCircle * 3), Quaternion.Euler(0, Random.Range(0f, 140f), Random.Range(0f, 360f)));
             newEnemy.transform.parent = _leafContainer.transform;
             _numTeaLeaves = _numTeaLeaves + 1;
         }
-        while (_numTeaLeaves3 < 220)
+        while (_numTeaLeaves3 < _startNumTeaLeaves3)
         {
 
             GameObject newEnemy = Instantiate(_teaLeaves, (Random.insideUnitCircle * Random.Range(0f, 2.3f)), Quaternion.Euler(0, Random.Range(0f, 140f), Random.Range(0f, 360f)));
             newEnemy.transform.parent = _leafContainer3.transform;
             _numTeaLeaves3 = _numTeaLeaves3 + 1;
-            if (_numTeaLeaves3 == 220)
+            if (_numTeaLeaves3 == _startNumTeaLeaves3)
             {
-                _leafContainer3.transform.position = new Vector3(Random.Range(-1f, 1.5f), Random.Range(-1.5f, 1f), 0);
+                _leafContainer3.transform.position = new Vector3(Random.Range(-1.3f, 1.3f), Random.Range(-1.3f, 1.3f), 0);
             }
         }
 
-        while (_numTeaLeaves2 < 50)
+        while (_numTeaLeaves2 < _startNumTeaLeaves2)
         {
 
             GameObject newEnemy = Instantiate(_teaLeaves2, (Random.insideUnitCircle * 1), Quaternion.Euler(0, Random.Range(0f, 40f), Random.Range(0f, 360f)));
             newEnemy.transform.parent = _leafContainer2.transform;
             _numTeaLeaves2 = _numTeaLeaves2 + 1;
-            if (_numTeaLeaves2 == 50)
+            if (_numTeaLeaves2 == _startNumTeaLeaves2)
             {
-                _leafContainer2.transform.position = new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), 0);
+                _leafContainer2.transform.position = new Vector3(Random.Range(0f, 4f), Random.Range(0f, 4f), 0);
             }
 
         }

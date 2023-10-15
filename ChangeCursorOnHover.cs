ec89f9d5231fe7b75470d9d8e9ef090f2d60ccd1
Julia Grimes <julia.s.grimes@gmail.com>
2023-10-15 10:46:15 -0400

    cursor changes on mouse enter, collider has to be at the correct z index

--- ChangeCursorOnHover.cs
+++ ChangeCursorOnHover.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class ChangeCursorOnHover : MonoBehaviour
 {
+    [SerializeField]
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
     // Start is called before the first frame update
-    void Start()
+    void OnMouseEnter()
     {
-        
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnMouseExit()
     {
-        
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+
     }
 }

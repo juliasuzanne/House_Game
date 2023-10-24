96dac4f1bd2af1b657c51c217bab1cdb238a41d9
Julia Grimes <julia.s.grimes@gmail.com>
2023-10-23 23:06:59 -0400

    Trying to add a script to make player speak on click

---
+++ SaySomethingOnClick.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaySomethingOnClick : MonoBehaviour
+{
+    private UIManager _uiManager;
+    private Transform player;
+    private Player playerScript;
+    [SerializeField]
+    private Text _playerText;
+    [SerializeField]
+    private string PlayerText;
+    private GameManager _gameManager;
+
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        _uiManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerScript = player.GetComponent<Player>();
+        _playerText = player.GetChild(0).GetChild(0).GetComponent<Text>();
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        _gameManager.PauseGame();
+        playerScript.MoveableFalse();
+        _playerText.gameObject.SetActive(true);
+        _playerText.text = PlayerText;
+        StartCoroutine("WaitForClick");
+
+
+    }
+
+    private IEnumerator WaitForClick()
+    {
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        _playerText.gameObject.SetActive(false);
+
+
+    }
+
+
+
+    void OnMouseUp()
+    {
+
+    }
+
+
+}

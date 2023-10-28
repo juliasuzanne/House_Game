bc54b9534d0f9f9c37009e2dbfc59ece5349a6f5
Julia Grimes <julia.s.grimes@gmail.com>
2023-10-27 22:19:00 -0400

    Modifying for in game use

--- PickColor.cs
+++ PickColor.cs
@@ -5,13 +5,41 @@ using UnityEngine;
 public class PickColor : MonoBehaviour
 {
     private SpriteRenderer sp;
-    private Color newColor;
+    private Color m_NewColor;
+    float m_Red, m_Blue, m_Green;
+
+    private Player player;
     // Start is called before the first frame update
     void Start()
     {
         sp = GetComponent<SpriteRenderer>();
         sp.color = Color.blue;
+        player = transform.GetComponent<Player>();
 
+    }
+
+    public void ChangeColor()
+    {
+
+        //Use the Sliders to manipulate the RGB component of Color
+        //Use the Label to identify the Slider
+        GUI.Label(new Rect(0, 30, 50, 30), "Red: ");
+        //Use the Slider to change amount of red in the Color
+        m_Red = GUI.HorizontalSlider(new Rect(35, 25, 200, 30), m_Red, 0, 1);
+
+        //The Slider manipulates the amount of green in the GameObject
+        GUI.Label(new Rect(0, 70, 50, 30), "Green: ");
+        m_Green = GUI.HorizontalSlider(new Rect(35, 60, 200, 30), m_Green, 0, 1);
+
+        //This Slider decides the amount of blue in the GameObject
+        GUI.Label(new Rect(0, 105, 50, 30), "Blue: ");
+        m_Blue = GUI.HorizontalSlider(new Rect(35, 95, 200, 30), m_Blue, 0, 1);
+
+        //Set the Color to the values gained from the Sliders
+        m_NewColor = new Color(m_Red, m_Green, m_Blue);
+
+        //Set the SpriteRenderer to the Color defined by the Sliders
+        sp.color = m_NewColor;
     }
 
 }

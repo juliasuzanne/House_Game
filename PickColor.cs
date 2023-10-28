cc48f00c1c4218091ba7734eb4ed97b5b2dc33e2
Julia Grimes <julia.s.grimes@gmail.com>
2023-10-27 22:13:55 -0400

    Add variable for color, testing changing color

--- PickColor.cs
+++ PickColor.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PickColor : MonoBehaviour
 {
     private SpriteRenderer sp;
+    private Color newColor;
     // Start is called before the first frame update
     void Start()
     {
         sp = GetComponent<SpriteRenderer>();
+        sp.color = Color.blue;
 
     }
 

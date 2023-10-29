c0fa63fa8c1139f9122014d389b197e46c4bba60
Julia Grimes <julia.s.grimes@gmail.com>
2023-10-28 21:20:47 -0400

    Change text content on click

---
+++ ChangeTextOnClick.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeTextOnClick : MonoBehaviour
+{
+
+  // private bool pencil = false;
+  private int count = 0;
+  private Text _text;
+
+  public string[] strings;
+  private SceneSwitch _sceneManager;
+
+  private bool finishConversation = false;
+
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+    _text = transform.gameObject.GetComponent<Text>();
+    _sceneManager = GameObject.Find("GameManager").GetComponent<SceneSwitch>();
+    Debug.Log(strings.Length);
+
+
+  }
+
+  void OnMouseDown()
+  {
+    if (count < strings.Length)
+    {
+      count = count + 1;
+      _text.text = strings[count];
+
+    }
+
+
+    else if (finishConversation == true)
+    {
+      _text.enabled = false;
+    }
+
+  }
+
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}

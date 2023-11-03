1bfdcde7b763b2fe84afe0588530715990843b2f
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-03 10:51:44 -0400

    Work on inventory, trying to get prefab to go into inventory

--- Archived/Inventory.cs
+++ Archived/Inventory.cs
@@ -7,20 +7,26 @@ public class Inventory : MonoBehaviour
    public bool[] isFull; // use to check if there is already an item in each slot
    public GameObject[] slots; // this to place items in the center of each slot, or drop
    public GameObject[] items; // 
-
+   private int count = 0;
    //variable gameObject item
-   public void AddItemToInventory()
+   public void AddItemToInventory(GameObject prefab)
    {
+      count = 0;
       foreach (bool full in isFull)
       {
-         if (full == true)
+         if (full == false)
          {
-            Debug.Log(full + " is TRUE");
+            Debug.Log(full + " is FALSE");
+            items[count] = prefab;
+            isFull[count] = true;
+            Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity, slots[count].transform);
+            break;
          }
          else
          {
-            Debug.Log(full + " is FALSE");
+            Debug.Log(full + " is TRUE");
          }
+         count = count + 1;
       }
    }
 

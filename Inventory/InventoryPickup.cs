56e1cfb625f0bfd3e4db6380d5b4a63d421f6e02
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-04 19:55:28 -0400

    Add inventory controller to outsource item control to singleton script, will pass through game object and control player inventory

--- Inventory/InventoryPickup.cs
+++ Inventory/InventoryPickup.cs
@@ -8,38 +8,45 @@ public class InventoryPickup : MonoBehaviour
 
     private float offsetX, offsetY;
     public static bool mouseButtonReleased = false;
-
+    private Transform _item;
     private Vector3 startingPos;
+    private Inventory _inventory;
 
     void Start()
     {
         startingPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        _inventory = GameObject.Find("Player").transform.GetComponent<Inventory>();
     }
 
     void Update()
     {
         if (mouseButtonReleased == true)
         {
+
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePosition.z = 99f;
-            transform.position = mousePosition;
+            _item.position = mousePosition;
+            if (Input.GetMouseButtonDown(0))
+            {
+                StopMoving();
+            }
 
         }
-        if (Input.GetMouseButtonDown(0))
-        {
-            StopMoving();
-        }
+
 
 
     }
     public void DragMouse()
     {
+        _item = this.transform;
         mouseButtonReleased = true;
     }
 
     private void StopMoving()
     {
+        _item = null;
         transform.position = startingPos;
         mouseButtonReleased = false;
+        Destroy(this.gameObject);
     }
 }

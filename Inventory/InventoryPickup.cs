e4397c567234346b8de1ced8abc287b8efefa93f
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-04 13:35:29 -0400

    Click and drag inventory button from panel, no interaction yet, just goes back to startingpos

--- Inventory/InventoryPickup.cs
+++ Inventory/InventoryPickup.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class InventoryPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector2 mousePosition;
+
+    private float offsetX, offsetY;
+    public static bool mouseButtonReleased = false;
+
+    private Vector3 startingPos;
+
     void Start()
     {
-        
+        startingPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (mouseButtonReleased == true)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 99f;
+            transform.position = mousePosition;
+
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            StopMoving();
+        }
+
+
+    }
+    public void DragMouse()
+    {
+        mouseButtonReleased = true;
+    }
+
+    void OnMouseDown()
+    {
+        mouseButtonReleased = false;
+    }
+
+    private void StopMoving()
+    {
+        transform.position = startingPos;
+        mouseButtonReleased = false;
     }
 }

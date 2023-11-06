bc8f40276bb7f2705369f0907eeec0ef7dc683cd
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-06 11:44:14 -0500

    Testing out multiple lines of thought script

--- Cave/Talk.cs
+++ Cave/Talk.cs
@@ -8,6 +8,7 @@ public class Talk : MonoBehaviour
     private Hand player;
     private GameObject platform;
     private string text;
+    private int count = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +21,27 @@ public class Talk : MonoBehaviour
 
     public void StepForward()
     {
+        switch (count)
+        {
+            case 0:
+                text = "What am I doing here?";
+                count = 1;
+                break;
+            case 1:
+                text = "I don't remember anything.";
+                count = 2;
+                break;
+            case 2:
+                text = "Is that me?";
+                count = 3;
+                break;
+            case 3:
+                transform.gameObject.SetActive(false);
+                platform.SetActive(false);
+                player.UseGravity();
+                break;
 
-        transform.gameObject.SetActive(false);
-        platform.SetActive(false);
-        player.UseGravity();
+        }
 
     }
 

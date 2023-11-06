d64e185de0b7fd56d9a85bde7c31dbf9c73c47e9
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-06 11:40:33 -0500

    Adding multiple thoughts to thought panel on pop up through Talk script

--- Cave/Talk.cs
+++ Cave/Talk.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Talk : MonoBehaviour
 {
     private Hand player;
     private GameObject platform;
+    private string text;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform.GetComponent<Hand>();
         platform = GameObject.Find("Platform");
+        text = transform.GetChild(0).GetComponent<Text>().text;
 
     }
 
-    public void ResumeGame()
+    public void StepForward()
     {
+
         transform.gameObject.SetActive(false);
         platform.SetActive(false);
         player.UseGravity();

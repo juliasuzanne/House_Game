bfb87461b61e3462406484a8c40a440cd00d0768
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-07 09:13:05 -0500

    spawn and play sound on trigger enter, might change to a text/audio manager object

--- Cave/PlaySoundOnCollision.cs
+++ Cave/PlaySoundOnCollision.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlaySoundOnCollision : MonoBehaviour
 {
+    private AudioSource _sound;
+    private Animator _animator;
+    private Transform _player;
+    private bool moving = false;
+    [SerializeField]
+    private float speed = 0.85f;
+    [SerializeField]
+    private GameObject _nextObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _sound = GetComponent<AudioSource>();
+        _animator = GetComponent<Animator>();
+        _player = GameObject.Find("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (moving == true)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        }
+
     }
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        moving = true;
+        _animator.SetTrigger("hit");
+        _sound.Play(0);
+        if (_nextObject != null)
+        {
+            Invoke("CreateNext", 3.5f);
+
+        }
+        Destroy(this.gameObject, 5f);
+
+    }
+
+    void CreateNext()
+    {
+        Instantiate(_nextObject, new Vector3(_player.position.x, _player.position.y, 90f), Quaternion.identity);
+    }
+
 }

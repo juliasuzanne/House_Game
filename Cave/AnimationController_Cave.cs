e8bc9943d5a3665b72ad1efe5a577e1dbf3a8745
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-08 13:24:00 -0500

    Make some simple methods for calling animations

--- Cave/AnimationController_Cave.cs
+++ Cave/AnimationController_Cave.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class AnimationController_Cave : MonoBehaviour
 {
+    private Animator _anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _anim = GetComponent<Animator>();
+        _anim.SetBool("Falling", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void AnimatorFalling()
+    {
+        _anim.SetBool("Falling", true);
+        _anim.SetBool("Walking", false);
+
+
+    }
+
+    public void AnimatorWalking()
+    {
+        _anim.SetBool("Walking", true);
+        _anim.SetBool("Falling", false);
+
+    }
+
+    public void AnimatorMakeFist()
+    {
+        _anim.SetTrigger("MakeFist");
+    }
+
+
 }

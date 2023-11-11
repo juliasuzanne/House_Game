c89147e8c5cae1869a382d8026f4161b36659562
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-11 14:00:55 -0500

    Simplifying and adjusting behavior of hint text spawning

--- Cave/HintTextManagerScript.cs
+++ Cave/HintTextManagerScript.cs
@@ -5,21 +5,55 @@ using UnityEngine;
 public class HintTextManagerScript : MonoBehaviour
 {
     [SerializeField]
-    private GameObject psstText;
-    [SerializeField]
-    private GameObject comeHereText;
-    [SerializeField]
-    private GameObject iCanHelpText;
+    private GameObject textObjectPrefab;
+    private Transform _player;
+    private bool _keepInstantiating = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _player = GameObject.Find("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (_keepInstantiating == true)
+        {
+            StartCoroutine("CreateText");
+        }
+
 
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _keepInstantiating = true;
+
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _keepInstantiating = false;
+
+        }
+
+    }
+
+    IEnumerator CreateText()
+    {
+        _keepInstantiating = false;
+        Instantiate(textObjectPrefab, new Vector3(_player.position.x + 2f, _player.position.y + 3f, 90f), Quaternion.identity);
+        yield return new WaitForSeconds(3f);
+        Destroy(textObjectPrefab, 4f);
+        _keepInstantiating = true;
+
+    }
+
 }
--- Cave/PlaySoundOnCollision.cs
+++ Cave/PlaySoundOnCollision.cs
@@ -5,28 +5,20 @@ using UnityEngine;
 public class PlaySoundOnCollision : MonoBehaviour
 {
     private AudioSource _sound;
-    private Transform _player;
     [SerializeField]
     private float speed = 0.85f;
 
     void Start()
     {
         _sound = GetComponent<AudioSource>();
-        _player = GameObject.Find("Player").transform;
+        _sound.Play(0);
+
     }
 
     void Update()
     {
 
         transform.Translate(Vector3.right * speed * Time.deltaTime);
-
-    }
-
-    public void LeadingText(GameObject prefab)
-    {
-        Instantiate(prefab, new Vector3(_player.position.x + 2f, _player.position.y + 3f, 90f), Quaternion.identity);
-        _sound.Play(0);
-        Destroy(this.gameObject, 5f);
 
     }
 

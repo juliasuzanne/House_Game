af9a2ef4b848459b188b3f0bc0f076b3bee26c0b
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-29 17:05:21 -0500

    Get reference to iris and position, create min and max values for movement

--- Test/Eyeball.cs
+++ Test/Eyeball.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Eyeball : MonoBehaviour
 {
+    private GameObject _iris;
+    private Vector2 _irisPos;
+    [SerializeField]
+    private float _xMin, _xMax;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _iris = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(_irisPos);
     }
 }

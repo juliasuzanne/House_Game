def02bb85717ca166aabc6d8d7eafb3e5141421f
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-29 17:41:27 -0500

    Calculations in for one iris to follow, trying to make it a prefab

--- Test/Eyeball.cs
+++ Test/Eyeball.cs
@@ -5,18 +5,51 @@ using UnityEngine;
 public class Eyeball : MonoBehaviour
 {
     private GameObject _iris;
-    private Vector2 _irisPos;
+    private Vector2 _mousePos;
     [SerializeField]
-    private float _xMin, _xMax;
+    private float _xScaler, _yScaler, _xMin, _xMax, _yMin, _yMax;
+    private float _distanceX, _distanceY, _irisPosx, _irisPosy, _newXPos, _newYPos;
+
     // Start is called before the first frame update
     void Start()
     {
         _iris = transform.GetChild(0).gameObject;
+        _irisPosx = _iris.transform.position.x;
+        _irisPosy = _iris.transform.position.y;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(_irisPos);
+        _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Debug.Log("_iris.transform.position.x: " + _iris.transform.position.x);
+
+        _distanceX = _iris.transform.position.x + _mousePos.x;
+        _distanceY = _iris.transform.position.y + _mousePos.y;
+        _newXPos = _irisPosx + _distanceX * _xScaler;
+        _newYPos = _irisPosy + _distanceY * _yScaler;
+        if (_newXPos < _xMin)
+        {
+            _newXPos = _xMin + 0.005f;
+        }
+        else if (_newXPos > _xMax)
+        {
+            _newXPos = _xMax - 0.005f;
+        }
+        if (_newYPos < _yMin)
+        {
+            _newYPos = _yMin + 0.005f;
+        }
+        else if (_newYPos > _yMax)
+        {
+            _newYPos = _yMax - 0.005f;
+        }
+
+        _newYPos = _irisPosy + _distanceY * _yScaler;
+        _iris.transform.position = new Vector2(_newXPos, _newYPos);
+
+
     }
 }

a891d69e2ef9c5773b95dd0ae02c75bf6da14185
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-29 20:31:28 -0500

    Follow with eyes on transform movement is complete, need to replace mouse pos with actual player when it is implemented

--- Test/GetDistance.cs
+++ Test/GetDistance.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class GetDistance : MonoBehaviour
 {
-    private float _originalX, _originalY, _distanceX, _distanceY, _parentX, _parentY;
+    private float _originalX, _originalY, _distanceX, _distanceY, _parentX, _parentY, _newPosX, _newPosY;
+
     [SerializeField]
-    private float _yScaler, _xScaler;
+    private float _yScaler, _xScaler, _xMin, _xMax, _yMin, _yMax;
     private Vector2 _newPos, _mousePos;
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,10 @@ public class GetDistance : MonoBehaviour
         _parentY = transform.parent.position.y;
         _originalX = transform.position.x;
         _originalY = transform.position.y;
+        _xMin = _originalX + _xMin;
+        _xMax = _originalX + _xMax;
+        _yMin = _originalY + _yMin;
+        _yMax = _originalY + _yMax;
 
     }
 
@@ -27,10 +32,30 @@ public class GetDistance : MonoBehaviour
         _distanceY = _mousePos.y - _originalY;
         Debug.Log(_distanceX + " " + _distanceY);
 
-        _newPos = new Vector2(_distanceX, _distanceY);
+        _newPosX = _parentX + _distanceX * _xScaler;
+        _newPosY = _parentY + _distanceY * _yScaler;
 
 
-        transform.position = new Vector2(_parentX + _newPos.x * _xScaler, _parentY + _newPos.y * _yScaler);
+        if (_newPosX < _xMin)
+        {
+            _newPosX = _xMin;
+        }
+        else if (_newPosX > _xMax)
+        {
+            _newPosX = _xMax;
+        }
+
+        if (_newPosY < _yMin)
+        {
+            _newPosY = _yMin;
+        }
+        else if (_newPosY > _yMax)
+        {
+            _newPosY = _yMax;
+        }
+
+
+        transform.position = new Vector2(_newPosX, _newPosY);
 
 
 
